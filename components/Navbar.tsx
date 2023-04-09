2b880b463db1ba804fb73cb808bbd00c38a86501
Andrii Naida <work.andrew.naida@gmail.com>
2023-04-09 18:57:36 +0200

    blog v2 (fixed icons)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,10 +16,14 @@ export const Navbar = ({ author }: { author: any; }) => {
                         <p>Young <span className={styles.styledText}>front-end</span> developer</p>
                     </div>
                 </div>
-                <div className={styles.socials}>
-                    <Link href="https://github.com/ddebixx"><GitHubIcon className={styles.icon}/></Link>
-                    <Link href="https://www.instagram.com/debix.cr2/"><Instagram className={styles.icon}/></Link>
-                    <Link href="https://www.linkedin.com/in/andrii-naida-74b610268/"><LinkedIn className={styles.icon}/></Link>
+                <div className={`${styles.socials}
+                
+                text-3xl
+                
+                max-md:text-2xl`}>
+                    <Link href="https://github.com/ddebixx"><GitHubIcon className={styles.icon} fontSize="inherit"/></Link>
+                    <Link href="https://www.instagram.com/debix.cr2/"><Instagram className={styles.icon} fontSize="inherit"/></Link>
+                    <Link href="https://www.linkedin.com/in/andrii-naida-74b610268/"><LinkedIn className={styles.icon} fontSize="inherit"/></Link>
                 </div>
             </div>
         </>
