14e69a87be60536e24eeac494420f29c1962515b
Andrii Naida <work.andrew.naida@gmail.com>
2023-04-12 16:03:54 +0200

    fallback-changed

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Slug.module.scss'
 import Image from 'next/image'
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { AnimatedParticles } from '@/components/AnimatedParticles';
-import { Nunito} from 'next/font/google'
+import { Nunito } from 'next/font/google'
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -11,7 +11,7 @@ const nunito = Nunito({
 })
 
 const graphcms = new GraphQLClient(
-    "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clg6idcnc0tde01t3cvzvgg5l/master"
+  "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clg6idcnc0tde01t3cvzvgg5l/master"
 );
 
 const QUERY = gql`
@@ -48,87 +48,87 @@ const SLUGLIST = gql`
 `;
 
 interface Post {
+  id: string;
+  title: string;
+  slug: string;
+  datePublished: string;
+  author: {
     id: string;
-    title: string;
-    slug: string;
-    datePublished: string;
-    author: {
-      id: string;
-      name: string;
-      avatar: {
-        url: string;
-      };
-    };
-    content: {
-      html: string;
-    };
-    coverPhoto: {
-      id: string;
+    name: string;
+    avatar: {
       url: string;
     };
+  };
+  content: {
+    html: string;
+  };
+  coverPhoto: {
+    id: string;
+    url: string;
+  };
+}
+
+interface SlugListData {
+  posts: {
+    slug: string;
+  }[];
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { posts } = await graphcms.request<SlugListData>(SLUGLIST);
+
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}) => {
+  if (!params?.slug) {
+    return { notFound: true };
   }
 
-  interface SlugListData {
-    posts: {
-      slug: string;
-    }[];
-  }
+  const slug = params.slug;
+  const { post } = await graphcms.request<{ post: Post }>(QUERY, { slug });
 
-  export const getStaticPaths: GetStaticPaths = async () => {
-    const { posts } = await graphcms.request<SlugListData>(SLUGLIST);
-  
-    const paths = posts.map((post) => ({
-      params: { slug: post.slug },
-    }));
-  
-    return {
-      paths,
-      fallback: false,
-    };
+  return {
+    props: {
+      post,
+    },
+    revalidate: 10,
   };
-  
-  export const getStaticProps: GetStaticProps = async ({
-    params,
-  }) => {
-    if (!params?.slug) {
-      return { notFound: true };
-    }
-  
-    const slug = params.slug;
-    const { post } = await graphcms.request<{ post: Post }>(QUERY, { slug });
-  
-    return {
-      props: {
-        post,
-      },
-      revalidate: 10,
-    };
-  };
+};
 
 export default function BlogPost({ post }: { post: Post }) {
-    return (
-        <>
-            <div className={`${styles.container} ${nunito.className}`}>
-                <main className={styles.blog}>
-                    <div>
-                        <Image src={post.coverPhoto.url} alt="" width={2000} height={2000} className={styles.cover} />
-                    </div>
+  return (
+    <>
+      <div className={`${styles.container} ${nunito.className}`}>
+        <main className={styles.blog}>
+          <div>
+            <Image src={post.coverPhoto.url} alt="" width={2000} height={2000} className={styles.cover} />
+          </div>
 
-                    <div className={styles.author}>
-                        <Image src={post.author.avatar.url} alt="" width={1000} height={1000} />
-                        <div className={styles.authtext}>
-                            <h6>By {post.author.name}</h6>
-                            <h6 className={styles.date}>{post.datePublished}</h6>
-                        </div>
-                    </div>
-                    <div className={styles.content}>
-                        <h2>{post.title}</h2>
-                        <div dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
-                    </div>
-                </main>
-                <AnimatedParticles />
+          <div className={styles.author}>
+            <Image src={post.author.avatar.url} alt="" width={1000} height={1000} />
+            <div className={styles.authtext}>
+              <h6>By {post.author.name}</h6>
+              <h6 className={styles.date}>{post.datePublished}</h6>
             </div>
+          </div>
+          <div className={styles.content}>
+            <h2>{post.title}</h2>
+            <div dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
+          </div>
+        </main>
+        <AnimatedParticles />
+      </div>
 
-        </>
-    )
+    </>
+  )
 }
